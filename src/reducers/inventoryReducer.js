f67efc7c601c34b7d827f77aadba71e4969353a0
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-09-28 04:56:49 +1300

    Update inventoryReducer.js

--- src/reducers/inventoryReducer.js
+++ src/reducers/inventoryReducer.js
@@ -1,1 +1,26 @@
+// src/reducers/inventoryReducer.js
+import { ADD_INVENTORY_ITEM, REMOVE_INVENTORY_ITEM } from '../actions/types';
+
+const initialState = {
+  inventory: [],
+};
+
+const inventoryReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_INVENTORY_ITEM:
+      return {
+        ...state,
+        inventory: [...state.inventory, action.payload],
+      };
+    case REMOVE_INVENTORY_ITEM:
+      return {
+        ...state,
+        inventory: state.inventory.filter((item) => item.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
+
+export default inventoryReducer;
 
