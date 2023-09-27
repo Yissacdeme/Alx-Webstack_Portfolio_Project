a4d7d91beb0bba427495551d2c72d1b74de7ab11
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-09-28 04:58:51 +1300

    Update inventoryActions.js

--- src/actions/inventoryActions.js
+++ src/actions/inventoryActions.js
@@ -1,1 +1,18 @@
+import { ADD_INVENTORY_ITEM, REMOVE_INVENTORY_ITEM } from './types';
+
+// Action creators
+export const addInventoryItem = (item) => {
+  return {
+    type: ADD_INVENTORY_ITEM,
+    payload: item,
+  };
+};
+
+export const removeInventoryItem = (itemId) => {
+  return {
+    type: REMOVE_INVENTORY_ITEM,
+    payload: itemId,
+  };
+};
+
 
