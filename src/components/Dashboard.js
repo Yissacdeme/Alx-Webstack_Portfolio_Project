897d4b013f328efa7e5c7dd091909b2e4d733be1
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-09-28 01:24:54 +1300

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,1 +1,22 @@
+// src/components/Dashboard.js
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Dashboard = () => {
+  const inventory = useSelector(state => state.inventory);
+
+  return (
+    <div>
+      <h1>Medical Device Inventory Dashboard</h1>
+      {/* Display inventory information */}
+      <ul>
+        {inventory.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Dashboard;
 
